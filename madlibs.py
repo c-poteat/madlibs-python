5d156f55b4c958b506843bd89df37b58c17e80b4
c-poteat <nellp7@yahoo.com>
2023-01-09 18:52:20 -0500

    First commit

---
+++ madlibs.py
@@ -0,0 +1,30 @@
+# Based on Back to the future
+year = input("Enter a year in yyyy format, and press enter:")
+firstname = input("Enter your first name:")
+verb_1 = input("Enter a verb of choice, and press enter:")
+verb_2 = input("Enter second verb of choice and press enter:")
+noun = input("Enter a noun of choice, and press enter:")
+adj_1 = input("Enter an adjective of choice, and press enter:")
+adj_2 = input("Enter a second adjective related to native people of a country, (Example Libyans)")
+year_2 = input("Enter a year in yyyy format, and press enter:")
+verb_3 = input("Enter a third verb of choice, and press enter:")
+verb_4 = input("Enter a fourth verb of choice, and press enter:")
+
+story = f"It is the year {year}. {firstname} McFly, a mild-mannered high school student, stopped by Dr. Emmett L. Brown's \
+laboratory to {verb_1} around with an amplifier. Then he receives a message from Doc that he needs {verb_2} from him for \
+Doc's latest {noun}, a time machine made out of a {adj_1} sports car that can travel through time instantaneously when it reaches a speed \
+velocity of 88 MPH. Then, Doc was gunned down by {adj_2} Nationalists, firstname makes an effort to escape from the {adj_2} \
+by using the time machine. Then {firstname} accidentally warps himself into {year_2} Where he meets both of his parents when they were \
+teenagers, then {firstname} unintentionally {verb_3} his parent's first meeting together, {firstname} then finds a younger version \
+of Doc and together they try to {verb_4} a way to get {firstname}'s parents-to-be back together, and to get {firstname} back to {year}."  
+print(story)
+
+
+# Original summary
+# It is the year 1985. Marty McFly, a mild-mannered high school student, stopped by Dr. Emmett L. Brown's 
+# laboratory to play around with an amplifier. Then he receives a message from Doc that he needs help from him 
+# for Doc's latest invention, a time machine made out of a DeLorean sports car that can travel through time instantaneously when it reaches a speed 
+# velocity of 88 MPH. Then, Doc was gunned down by Libyan Nationalists, Marty makes an effort to escape from the Lybians 
+# by using the time machine. Then Marty accidentally warps himself into 1955. Where he meets both of his parents when they were 
+# teenagers, then Marty unintentionally interrupts his parent's first meeting together, he then finds a younger version 
+# of Doc and together they try to find a way to get Marty's parents-to-be back together, and to get Marty back to 1985. - John Wiggins
